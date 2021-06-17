6858314467577bc2c49a453ca61684e8b4640aa0
Niels Boogaard <niels.boogaard@luminis.eu>
2021-06-17 11:51:21 +0200

    Bumped versions of dependencies and published release 1.0.8

--- dist/filepond-plugin-manage-metadata.esm.js
+++ dist/filepond-plugin-manage-metadata.esm.js
@@ -1,5 +1,5 @@
 /*!
- * FilePondPluginManageMetadata 1.0.7
+ * FilePondPluginManageMetadata 1.0.8
  * Licensed under MIT, https://opensource.org/licenses/MIT/
  * Please visit undefined for details.
  */
